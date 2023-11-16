7ab202e0af8e27ae8009112ab61ab93ea957d3ff
tanmtn <tanmtni@gmail.com>
2023-11-16 23:08:25 +0900

    feat:Footer 기능 추가

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+    return (
+        <footer className="footer footer-center p-10 bg-primary text-primary-content text-center">
+            <aside>
+                <p className="font-bold">
+                    ACME Industries Ltd. <br />
+                    Providing reliable tech since 1992
+                </p>
+                <p>Copyright © 2023 - All right reserved</p>
+            </aside>
+        </footer>
+    );
+}
+
+export default Footer;
