4e5530b3167abfe7a6d3d8f6feb4b0bf79a13caf
byungyong <quddyddlrk@naver.com>
2023-11-17 11:00:58 +0900

    feat:add Outlet
    react-router-dom Outlet을 이용하여 중첩 라우팅 레이아웃 적용

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,15 @@
-import Footer from './Footer';
-import Header from './Header';
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+import Header from "./Header";
 
-function Layout({ children }) {
-    return (
-        <>
-            <Header />
-            {children}
-            <Footer />
-        </>
-    );
+function Layout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
 }
 
 export default Layout;
