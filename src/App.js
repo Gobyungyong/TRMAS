04bfbaa2836f935b754bec76182f3b684ab37588
byungyong <quddyddlrk@naver.com>
2023-11-17 11:02:27 +0900

    feat:add admin router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Layout from './components/Layout';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import routes from "./routes";
+import Layout from "./components/Layout";
+import Admin from "./pages/Admin";
 
 function App() {
-    return (
-        <>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/test" element={<Layout />} />
-                </Routes>
-            </BrowserRouter>
-        </>
-    );
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path={routes.index} element={<Layout />}>
+            <Route path={routes.admin} element={<Admin />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
