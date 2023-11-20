dd64bdbe7f5942f7112ea2f52aa52d18ffc6edc7
tanmtn <tanmtni@gmail.com>
2023-11-20 20:13:55 +0900

    feat:home page 생성,css 하는중..

---
+++ src/pages/Home.jsx
@@ -0,0 +1,44 @@
+function Home() {
+    return (
+        <div>
+            <div className="bg-gray-600 h-screen w-screen">
+                <div className="text-white text-center">Rejuvenating vacant houses with youth and vitality</div>
+                <div>
+                    <div className="flex justify-center bg-white w-100% h-96 ">
+                        <div>
+                            <div className="text-center text-3xl">SERVICE</div>
+                            <div className="flex space-x-60">
+                                <div className="text-center">
+                                    <div className="text-3xl pb-5">3D modeling</div>
+                                    <span>Accurate</span>
+                                    <br />
+                                    <span>modeling based</span>
+                                    <br />
+                                    <span>on measured</span>
+                                    <br />
+                                    <span>data</span>
+                                    <br />
+                                    <span>BIM</span>
+                                </div>
+                                <div className="text-center">
+                                    <div className="text-3xl pb-5">projection mapping</div>
+                                    <span>Leverages various</span>
+                                    <br />
+                                    <span>media from</span>
+                                    <br />
+                                    <span>building facade</span>
+                                    <br />
+                                    <span>based on 3d</span>
+                                    <br />
+                                    <span>modeling</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
