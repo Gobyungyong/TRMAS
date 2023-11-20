6cff02a749e47410ffb2feb19d170b3f2d054ede
tanmtn <tanmtni@gmail.com>
2023-11-20 20:10:12 +0900

    feat:home route 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import routes from "./routes";
-import Layout from "./components/Layout";
-import Admin from "./pages/admin/Admin";
-import Story from "./pages/admin/Stroy";
-import Policy from "./pages/admin/Policy";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import routes from './routes';
+import Layout from './components/Layout';
+import Admin from './pages/admin/Admin';
+import Story from './pages/admin/Stroy';
+import Policy from './pages/admin/Policy';
+import Home from './pages/Home';
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path={routes.index} element={<Layout />}>
-            <Route path={routes.admin} element={<Admin />} />
-            <Route path={routes.storyAdmin} element={<Story />} />
-            <Route path={routes.policyAdmin} element={<Policy />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={routes.index} element={<Layout />}>
+                        <Route path={routes.admin} element={<Admin />} />
+                        <Route path={routes.storyAdmin} element={<Story />} />
+                        <Route path={routes.policyAdmin} element={<Policy />} />
+                        <Route path={routes.home} element={<Home />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
