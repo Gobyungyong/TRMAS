18ba05fe98f47e4f7a19d122165b4eda166e2d44
tanmtn <tanmtni@gmail.com>
2023-11-23 18:11:23 +0900

    feat:home css 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,16 @@
 function Home() {
     return (
         <div>
-            <div className="bg-gray-600 h-screen w-screen">
+            <div className="bg-gray-600 h-screen w-screen bg-fixed">
                 <div className="text-white text-center">Rejuvenating vacant houses with youth and vitality</div>
-                <div>
-                    <div className="flex justify-center bg-white w-100% h-96 ">
+                <section>
+                    <div className="lg:flex lg:justify-center bg-white w-100% lg:h-96 pt-10 pb-10 ">
                         <div>
-                            <div className="text-center text-3xl">SERVICE</div>
-                            <div className="flex space-x-60">
-                                <div className="text-center">
-                                    <div className="text-3xl pb-5">3D modeling</div>
+                            <div className="text-center text-xl md:text-2xl lg:text-3xl mb-4">SERVICE</div>
+                            <div className="w-10 relative left-56 border-b-2 border-b-gray-950 border-solid"></div>
+                            <div className="lg:flex lg:space-x-60">
+                                <div className="text-center mt-10">
+                                    <div className="text-xl md:text-2xl lg:text-3xl pb-5">3D modeling</div>
                                     <span>Accurate</span>
                                     <br />
                                     <span>modeling based</span>
@@ -20,8 +21,8 @@ function Home() {
                                     <br />
                                     <span>BIM</span>
                                 </div>
-                                <div className="text-center">
-                                    <div className="text-3xl pb-5">projection mapping</div>
+                                <div className="text-center mt-10">
+                                    <div className="text-xl md:text-2xl lg:text-3xl pb-5">projection mapping</div>
                                     <span>Leverages various</span>
                                     <br />
                                     <span>media from</span>
@@ -35,7 +36,37 @@ function Home() {
                             </div>
                         </div>
                     </div>
-                </div>
+                </section>
+                <section>
+                    <div className="lg:flex lg:justify-center bg-slate-400 w-100% lg:h-72 pt-10 pb-10 ">
+                        <div>
+                            <div className="text-center text-xl md:text-2xl lg:text-3xl mb-4">DIGITAL TWIN DATA</div>
+                            <div className="w-10 relative left-56 border-b-2 border-b-gray-950 border-solid"></div>
+                            <div className="lg:flex lg:space-x-60">
+                                <div className="text-center mt-10">
+                                    <div className="text-xl md:text-2xl lg:text-3xl pb-5">BIM</div>
+                                </div>
+                                <div className="text-center mt-10">
+                                    <div className="text-xl md:text-2xl lg:text-3xl pb-5">
+                                        3D
+                                        <br />
+                                        scanning
+                                    </div>
+                                </div>
+                                <div className="text-center mt-10">
+                                    <div className="text-xl md:text-2xl lg:text-3xl pb-5">
+                                        Reality
+                                        <br />
+                                        Capture
+                                    </div>
+                                </div>
+                                <div className="text-center mt-10">
+                                    <div className="text-xl md:text-2xl lg:text-3xl pb-5">drone</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
         </div>
     );
