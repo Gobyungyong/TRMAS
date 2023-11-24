b3df1de31b33fc056b057983593128d07d279b2e
tanmtn <tanmtni@gmail.com>
2023-11-24 17:14:46 +0900

    feat:home css 큰 틀 잡기

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,73 +1,94 @@
 function Home() {
     return (
         <div>
-            <div className="bg-gray-600 h-screen w-screen bg-fixed">
+            <div className="bg-gray-600 h-[502px] w-full m-0">
                 <div className="text-white text-center">Rejuvenating vacant houses with youth and vitality</div>
-                <section>
-                    <div className="lg:flex lg:justify-center bg-white w-100% lg:h-96 pt-10 pb-10 ">
-                        <div>
-                            <div className="text-center text-xl md:text-2xl lg:text-3xl mb-4">SERVICE</div>
-                            <div className="w-10 relative left-56 border-b-2 border-b-gray-950 border-solid"></div>
-                            <div className="lg:flex lg:space-x-60">
-                                <div className="text-center mt-10">
-                                    <div className="text-xl md:text-2xl lg:text-3xl pb-5">3D modeling</div>
-                                    <span>Accurate</span>
-                                    <br />
-                                    <span>modeling based</span>
-                                    <br />
-                                    <span>on measured</span>
-                                    <br />
-                                    <span>data</span>
-                                    <br />
-                                    <span>BIM</span>
-                                </div>
-                                <div className="text-center mt-10">
-                                    <div className="text-xl md:text-2xl lg:text-3xl pb-5">projection mapping</div>
-                                    <span>Leverages various</span>
-                                    <br />
-                                    <span>media from</span>
-                                    <br />
-                                    <span>building facade</span>
-                                    <br />
-                                    <span>based on 3d</span>
-                                    <br />
-                                    <span>modeling</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <section>
-                    <div className="lg:flex lg:justify-center bg-slate-400 w-100% lg:h-72 pt-10 pb-10 ">
-                        <div>
-                            <div className="text-center text-xl md:text-2xl lg:text-3xl mb-4">DIGITAL TWIN DATA</div>
-                            <div className="w-10 relative left-56 border-b-2 border-b-gray-950 border-solid"></div>
-                            <div className="lg:flex lg:space-x-60">
-                                <div className="text-center mt-10">
-                                    <div className="text-xl md:text-2xl lg:text-3xl pb-5">BIM</div>
-                                </div>
-                                <div className="text-center mt-10">
-                                    <div className="text-xl md:text-2xl lg:text-3xl pb-5">
-                                        3D
-                                        <br />
-                                        scanning
-                                    </div>
-                                </div>
-                                <div className="text-center mt-10">
-                                    <div className="text-xl md:text-2xl lg:text-3xl pb-5">
-                                        Reality
-                                        <br />
-                                        Capture
-                                    </div>
-                                </div>
-                                <div className="text-center mt-10">
-                                    <div className="text-xl md:text-2xl lg:text-3xl pb-5">drone</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
             </div>
+            <section>
+                <div className="flex justify-center bg-white w-100% pt-10 pb-10 ">
+                    <div className="text-center text-xl md:text-2xl lg:text-3xl border-b-2 border-solid border-b-gray-950 h-12 justify-center">
+                        SERVICE
+                    </div>
+                </div>
+                <div className="flex justify-center bg-white w-100% h-[500px] sm:h-[530px] md:h-[550px] lg:h-[300px] pb-10 ">
+                    <div className="lg:flex lg:space-x-60">
+                        <div className="text-center mt-10">
+                            <div className="text-xl md:text-2xl lg:text-3xl pb-5">3D modeling</div>
+                            <span>Accurate</span>
+                            <br />
+                            <span>modeling based</span>
+                            <br />
+                            <span>on measured</span>
+                            <br />
+                            <span>data</span>
+                            <br />
+                            <span>BIM</span>
+                        </div>
+                        <div className="text-center mt-10">
+                            <div className="text-xl md:text-2xl lg:text-3xl pb-5">projection mapping</div>
+                            <span>Leverages various</span>
+                            <br />
+                            <span>media from</span>
+                            <br />
+                            <span>building facade</span>
+                            <br />
+                            <span>based on 3d</span>
+                            <br />
+                            <span>modeling</span>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div className="flex justify-center bg-slate-400 w-100% pt-10 pb-10 ">
+                    <div className="text-center text-xl md:text-2xl lg:text-3xl border-b-2 border-solid border-b-gray-950 h-12 justify-center">
+                        DIGITAL TWIN DATA
+                    </div>
+                </div>
+                <div className="flex justify-center bg-slate-400 w-100% h-[500px] sm:h-[530px] md:h-[550px] lg:h-[300px]  pb-10 ">
+                    <div className="lg:flex lg:space-x-60 pt-10">
+                        <div className="text-center mt-10">
+                            <div className="text-lg md:text-xl lg:text-2xl pb-5">BIM</div>
+                        </div>
+                        <div className="text-center mt-10">
+                            <div className="ext-lg md:text-xl lg:text-2xl pb-5">
+                                3D
+                                <br />
+                                Scanning
+                            </div>
+                        </div>
+                        <div className="text-center mt-10">
+                            <div className="ext-lg md:text-xl lg:text-2xl pb-5">
+                                Reality
+                                <br />
+                                Capture
+                            </div>
+                        </div>
+                        <div className="text-center mt-10">
+                            <div className="ext-lg md:text-xl lg:text-2xl pb-5">Drone</div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div className="flex justify-center bg-white w-100% pt-10 pb-10 ">
+                    <div className="text-center text-xl md:text-2xl lg:text-3xl border-b-2 border-solid border-b-gray-950 pb-3">
+                        CONTACT
+                    </div>
+                </div>
+                <div className="flex justify-center bg-white w-100% pb-10 ">
+                    <div className="lg:flex lg:space-x-10 pt-20">
+                        <div className="w-96 h-52 bg-slate-500"></div>
+                        <div className="text-center mt-10">
+                            <div className="text-left text-sm md:text-md lg:text-lg">
+                                Tel : 010-5659-2218
+                                <br />
+                                Email : jnk2218@naver.com
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 }
