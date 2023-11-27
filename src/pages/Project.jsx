44d216aa0f88477fe32533d89bb6027bcb63e375
byungyong <quddyddlrk@naver.com>
2023-11-27 14:34:22 +0900

    feat:add button icon

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,3 +1,4 @@
+import { PencilIcon } from "@heroicons/react/24/outline";
 import Button from "../components/Button";
 
 function Project({ template = null }) {
@@ -10,13 +11,23 @@ function Project({ template = null }) {
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Project
           </h2>
-          {template === "admin" ? <Button name="등록" /> : null}
+          {template === "admin" ? (
+            <Button
+              name="등록"
+              icon={
+                <PencilIcon
+                  className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              }
+            />
+          ) : null}
         </div>
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {projects.map((project, i) => (
             <article
               key={i}
-              className="flex max-w-xl flex-col items-start justify-between bg-slate-500  h-96"
+              className="flex max-w-xl flex-col items-start justify-between bg-slate-500 h-96"
             ></article>
           ))}
         </div>
