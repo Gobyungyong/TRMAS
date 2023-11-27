90f2a5e08fed687c4d1cf10ca5c7d4813944ef84
byungyong <quddyddlrk@naver.com>
2023-11-27 14:33:57 +0900

    feat:add icon props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,10 @@
-import { PencilIcon } from "@heroicons/react/24/outline";
-
-function Button({ name, onClick }) {
+function Button({ name, icon, onClick }) {
   return (
     <button
       className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
       onClick={onClick}
     >
-      <PencilIcon
-        className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400"
-        aria-hidden="true"
-      />
+      {icon}
       {name}
     </button>
   );
