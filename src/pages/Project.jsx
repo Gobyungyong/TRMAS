e151ae62f9143dadc54abf034a1eccf157156c03
byungyong <quddyddlrk@naver.com>
2023-11-27 14:25:26 +0900

    feat:add Button component

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,4 +1,4 @@
-import { PencilIcon } from "@heroicons/react/24/outline";
+import Button from "../components/Button";
 
 function Project({ template = null }) {
   const projects = [1, 2, 3, 4, 5, 6, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5];
@@ -10,13 +10,7 @@ function Project({ template = null }) {
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Project
           </h2>
-          <button className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-            <PencilIcon
-              className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400"
-              aria-hidden="true"
-            />
-            등록
-          </button>
+          {template === "admin" ? <Button name="등록" /> : null}
         </div>
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {projects.map((project, i) => (
