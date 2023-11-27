29d1ff83bea0630b3309d29c488acfb98193af44
byungyong <quddyddlrk@naver.com>
2023-11-27 21:47:24 +0900

    chore:install chakra UI

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RecoilRoot } from "recoil";
+import { ChakraProvider } from "@chakra-ui/react";
 import "tailwindcss/tailwind.css";
 import App from "./App";
-import { RecoilRoot } from "recoil";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RecoilRoot>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </RecoilRoot>
   </React.StrictMode>
 );
