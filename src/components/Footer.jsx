0a0b1b2f65f0c5b4f2e1dedcb5171782b2ef8deb
byungyong <quddyddlrk@naver.com>
2023-11-28 10:42:09 +0900

    style:add border top

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,15 @@
 function Footer() {
-    return (
-        <footer className="footer footer-center p-10 bg-primary text-primary-content text-center">
-            <aside>
-                <p className="font-bold">
-                    ACME Industries Ltd. <br />
-                    Providing reliable tech since 1992
-                </p>
-                <p>Copyright © 2023 - All right reserved</p>
-            </aside>
-        </footer>
-    );
+  return (
+    <footer className="footer footer-center p-10 bg-primary text-primary-content text-center border-t border-gray-200">
+      <aside>
+        <p className="font-bold">
+          ACME Industries Ltd. <br />
+          Providing reliable tech since 1992
+        </p>
+        <p>Copyright © 2023 - All right reserved</p>
+      </aside>
+    </footer>
+  );
 }
 
 export default Footer;
