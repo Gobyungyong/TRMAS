150b30bba65937e2f3213ee1f273e0a677e14eed
byungyong <quddyddlrk@naver.com>
2023-11-28 11:39:29 +0900

    feat:add modal

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -2,12 +2,18 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { PencilIcon } from "@heroicons/react/24/outline";
 import { ref, onValue } from "firebase/database";
+import { SwiperSlide } from "swiper/react";
+
 import routes from "../routes";
 import Button from "../components/Button";
 import { db } from "../firebase";
+import Modal from "../components/Modal";
+import Swiper from "../components/Swiper";
 
 function Project({ template = null }) {
   const [projects, setProjects] = useState([]);
+  const [project, setProject] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -17,6 +23,11 @@ function Project({ template = null }) {
       setProjects(projectsData);
     });
   }, []);
+
+  function toggleIsOpen(project) {
+    setIsOpen((prev) => !prev);
+    setProject(project);
+  }
 
   return (
     <div className="bg-white py-8">
@@ -40,18 +51,43 @@ function Project({ template = null }) {
         </div>
         <div className="mx-auto mt-10 grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {projects.map((project, i) => (
-            <div className="max-w-l overflow-hidden rounded-sm shadow-md shadow-black relative group">
+            <div
+              onClick={() => toggleIsOpen(project)}
+              className="max-w-l overflow-hidden rounded-sm shadow-md shadow-black relative group"
+            >
               <img
                 src={project.images[0]}
                 alt={project.subject}
                 key={i}
-                className="max-w-l items-center bg-slate-400 h-96 brightness-100 duration-150 hover:brightness-75 hover:scale-110"
+                className="max-w-l items-center bg-slate-400 h-96 brightness-100 duration-150 group-hover:brightness-75 group-hover:scale-110"
               />
               <div className="group-hover:block group-hover:text-xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 group-hover:text-white group-hover:z-50 group-hover:font-bold group-hover:text-opacity-80 hidden">
                 {project.subject}
               </div>
+              <div className="group-hover:block group-hover:text-xl absolute top-5 right-5 space-x-4 group-hover:text-white group-hover:z-50 group-hover:font-bold group-hover:text-opacity-80 hidden">
+                <button>수정</button>
+                <button>삭제</button>
+              </div>
             </div>
           ))}
+          <Modal
+            title={project?.subject}
+            size="xl"
+            isOpen={isOpen}
+            onClose={toggleIsOpen}
+          >
+            <Swiper>
+              {project?.images?.map((image, i) => (
+                <SwiperSlide key={i} className="relative">
+                  <img
+                    className="w-full h-full max-w-xl object-cover overflow-hidden"
+                    src={image}
+                    alt={`${image}-${i}`}
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </Modal>
         </div>
       </div>
     </div>
