08f8fc1cf62efbebe9316b032836d1ce610bbb7c
byungyong <quddyddlrk@naver.com>
2023-11-29 13:25:53 +0900

    feat:add atoms for quill editor

--- src/atoms.js
+++ src/atoms.js
@@ -4,3 +4,13 @@ export const isAdminState = atom({
   key: "isAdminState",
   default: null,
 });
+
+export const quillValue = atom({
+  key: "quillValue",
+  default: "",
+});
+
+export const quillImages = atom({
+  key: "quillImages",
+  default: [],
+});
