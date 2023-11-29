65a97f22f9294a53d559763ffb87a2b9fe7fadfd
byungyong <quddyddlrk@naver.com>
2023-11-29 13:26:43 +0900

    fix:fix project list when project not exist

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -19,6 +19,7 @@ function Project({ template = null }) {
   useEffect(() => {
     const projectsRef = ref(db, "projects/");
     onValue(projectsRef, (snapshot) => {
+      if (!snapshot.val()) return;
       const projectsData = Object.values(snapshot.val());
       setProjects(projectsData);
     });
@@ -60,33 +61,40 @@ function Project({ template = null }) {
           ) : null}
         </div>
         <div className="mx-auto mt-10 grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {projects.map((project, i) => (
-            <div
-              key={i}
-              className="max-w-l overflow-hidden rounded-sm shadow-md shadow-black relative z-40 group cursor-pointer"
-            >
-              <img
-                src={project.images[0]}
-                alt={project.subject}
-                onClick={() => toggleIsOpen(project)}
-                key={i}
-                className="max-w-l items-center bg-slate-400 h-96 brightness-100 duration-150 group-hover:brightness-75 group-hover:scale-110"
-              />
-
-              <div
-                onClick={() => toggleIsOpen(project)}
-                className="group-hover:block group-hover:text-xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 group-hover:text-white group-hover:z-40 group-hover:font-bold group-hover:text-opacity-80 hidden"
-              >
-                {project.subject}
-              </div>
-              {template === "admin" ? (
-                <div className="group-hover:block group-hover:text-xl absolute top-5 right-5 space-x-4 group-hover:text-white group-hover:z-50 group-hover:font-bold group-hover:text-opacity-80 hidden">
-                  <button onClick={() => modifyProject(project)}>수정</button>
-                  <button onClick={() => deleteProject(project)}>삭제</button>
-                </div>
-              ) : null}
+          {projects.length === 0 ? (
+            <div className="text-center w-full h-screen">
+              아직 프로젝트가 존재하지 않습니다.
             </div>
-          ))}
+          ) : (
+            projects?.map((project, i) => (
+              <div
+                key={i}
+                className="max-w-l overflow-hidden rounded-sm shadow-md shadow-black relative z-40 group cursor-pointer"
+              >
+                <img
+                  src={project.images[0]}
+                  alt={project.subject}
+                  onClick={() => toggleIsOpen(project)}
+                  key={i}
+                  className="max-w-l items-center bg-slate-400 h-96 brightness-100 duration-150 group-hover:brightness-75 group-hover:scale-110"
+                />
+
+                <div
+                  onClick={() => toggleIsOpen(project)}
+                  className="group-hover:block group-hover:text-xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 group-hover:text-white group-hover:z-40 group-hover:font-bold group-hover:text-opacity-80 hidden"
+                >
+                  {project.subject}
+                </div>
+                {template === "admin" ? (
+                  <div className="group-hover:block group-hover:text-xl absolute top-5 right-5 space-x-4 group-hover:text-white group-hover:z-50 group-hover:font-bold group-hover:text-opacity-80 hidden">
+                    <button onClick={() => modifyProject(project)}>수정</button>
+                    <button onClick={() => deleteProject(project)}>삭제</button>
+                  </div>
+                ) : null}
+              </div>
+            ))
+          )}
+
           <Modal
             title={project?.subject}
             size="xl"
