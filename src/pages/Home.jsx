e7f4703ef45721761c47a2a5b53d1be34fef69bd
tanmtn <tanmtni@gmail.com>
2023-12-12 23:18:35 +0900

    style:add home style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,24 @@
+import React from 'react';
+import './animation.css';
 function Home() {
     return (
         <div>
-            <div className="bg-gray-600 h-[502px] w-full m-0">
-                <div className="text-white text-center">Rejuvenating vacant houses with youth and vitality</div>
+            <div
+                className="h-[502px] w-full m-0 flex items-center justify-center"
+                style={{
+                    backgroundImage: "url('/main.png')",
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundAttachment: 'fixed',
+                    height: '870px',
+                }}
+            >
+                <div
+                    className="text-white text-center animate-fade-in text-6xl"
+                    style={{ animationDuration: '6s', animationDelay: '0.3s' }}
+                >
+                    Rejuvenating vacant houses with youth and vitality
+                </div>
             </div>
             <section>
                 <div className="flex justify-center bg-white w-100% pt-10 pb-10 ">
@@ -40,37 +56,41 @@ function Home() {
                 </div>
             </section>
             <section>
-                <div className="flex justify-center bg-slate-400 w-100% pt-10 pb-10 ">
-                    <div className="text-center text-xl md:text-2xl lg:text-3xl border-b-2 border-solid border-b-gray-950 h-12 justify-center">
+                <div
+                    className="w-100% pt-10 pb-10 "
+                    style={{ backgroundImage: "url('/2.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}
+                >
+                    <div className="text-center text-xl md:text-2xl lg:text-3xl border-b-2 border-solid border-b-white h-12 justify-center text-white font-semibold">
                         DIGITAL TWIN DATA
                     </div>
-                </div>
-                <div className="flex justify-center bg-slate-400 w-100% h-[500px] sm:h-[530px] md:h-[550px] lg:h-[300px]  pb-10 ">
-                    <div className="lg:flex lg:space-x-60 pt-10">
-                        <div className="text-center mt-10">
-                            <div className="text-lg md:text-xl lg:text-2xl pb-5">BIM</div>
-                        </div>
-                        <div className="text-center mt-10">
-                            <div className="ext-lg md:text-xl lg:text-2xl pb-5">
-                                3D
-                                <br />
-                                Scanning
+
+                    <div className="flex justify-center w-100% h-[500px] sm:h-[530px] md:h-[550px] lg:h-[300px]  pb-10 ">
+                        <div className="lg:flex lg:space-x-60 pt-10">
+                            <div className="text-center mt-10">
+                                <div className="text-lg md:text-xl lg:text-2xl pb-5 text-white font-semibold">BIM</div>
                             </div>
-                        </div>
-                        <div className="text-center mt-10">
-                            <div className="ext-lg md:text-xl lg:text-2xl pb-5">
-                                Reality
-                                <br />
-                                Capture
+                            <div className="text-center mt-10">
+                                <div className="ext-lg md:text-xl lg:text-2xl pb-5 text-white font-semibold">
+                                    3D
+                                    <br />
+                                    Scanning
+                                </div>
                             </div>
-                        </div>
-                        <div className="text-center mt-10">
-                            <div className="ext-lg md:text-xl lg:text-2xl pb-5">Drone</div>
+                            <div className="text-center mt-10">
+                                <div className="ext-lg md:text-xl lg:text-2xl pb-5 text-white font-semibold">
+                                    Reality
+                                    <br />
+                                    Capture
+                                </div>
+                            </div>
+                            <div className="text-center mt-10">
+                                <div className="ext-lg md:text-xl lg:text-2xl pb-5 text-white font-semibold">Drone</div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </section>
-            <section>
+            {/* <section>
                 <div className="flex justify-center bg-white w-100% pt-10 pb-10 ">
                     <div className="text-center text-xl md:text-2xl lg:text-3xl border-b-2 border-solid border-b-gray-950 pb-3">
                         CONTACT
@@ -88,7 +108,7 @@ function Home() {
                         </div>
                     </div>
                 </div>
-            </section>
+            </section> */}
         </div>
     );
 }
